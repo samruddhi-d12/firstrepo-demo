4b6b1435b337c18ba2c43026508b95d7595af169
samruddhi-d12 <samruddhidadas12@gmail.com>
2026-06-23 20:51:05 +0530

    Add ClassAndObject with main method and name field

---
+++ OOPS/ClassAndObject.java
@@ -0,0 +1,12 @@
+public class ClassAndObject {
+
+    String name = "Samruddhi";
+
+    public static void main(String[] args) {
+
+        ClassAndObject s1 = new ClassAndObject();
+
+        System.out.println(s1.name);
+
+    }
+}
